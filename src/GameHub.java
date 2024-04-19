156438e4d9023d62433e9139a7f6364b90d2006b
lmoore234 <123116702+lmoore234@users.noreply.github.com>
2024-04-19 10:25:56 -0500

    Update GameHub.java

--- src/GameHub.java
+++ src/GameHub.java
@@ -1,6 +1,33 @@
+import java.awt.Color;
+
+import edu.macalester.graphics.CanvasWindow;
+import edu.macalester.graphics.Ellipse;
+import edu.macalester.graphics.Rectangle;
+
+
 public class GameHub {
+<<<<<<< Updated upstream
 
     public GameHub(){
 
+=======
+<<<<<<< HEAD
+    
+
+
+
+    public static final int CANVAS_HEIGHT = 1000;
+    public static final int CANVAS_WIDTH = 800;
+
+
+
+    public static void main(String[] args) {
+        CanvasWindow canvas = new CanvasWindow("the hub", CANVAS_HEIGHT, CANVAS_WIDTH);
+=======
+
+    public GameHub(){
+
+>>>>>>> 5cf7e5a4ae05cf57cf11ac7d3c4061ef58eb0bab
+>>>>>>> Stashed changes
     }
 }
